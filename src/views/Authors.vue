16dcef316018801e54da091c5681b79e179626b6
Satya Perry <s.dog@satyadarshin.com>
2020-05-16 15:34:33 +0100

    Developed data files and inserted foregin keys. new Masterworks list in process

--- src/views/Authors.vue
+++ src/views/Authors.vue
@@ -25,6 +25,9 @@ export default {
     appAuthorTable: authorTable,
     appAuthorSpotlight: authorSpotlight
   },
+  computed: {
+
+  },
   mounted() {
     awardDataService.fetchTextRecords('authors')
     .then((jsonData) => {
