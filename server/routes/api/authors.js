6c4f829b78d89d8d6ab24fc420a5d612608e9180
Satya Perry <s.dog@satyadarshin.com>
2020-07-14 18:42:36 +0100

    Inserted basic functioning Express extension: confirms api is being reached. Next to get the Vue app and express to correspind via a service

---
+++ server/routes/api/authors.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get authors
+router.get('/', async (req, res) => {
+  // res.send('go, flight');
+  const authors = await loadAuthorsCollection();
+  res.send(await authors.find({}).toArray());
+});
+
+// Add author
+// router.author('/', async (req, res) => {
+//   const authors = await loadAuthorsCollection();
+//   await authors.insertOne({
+//     text: req.body.text,
+//     createdAt: new Date()
+//   });
+//   res.status(201).send();
+// });
+
+// // Delete author
+// router.delete('/:id', async (req, res) => {
+//   const authors = await loadAuthorsCollection();
+//   await authors.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+//   res.status(200).send({});
+// });
+
+async function loadAuthorsCollection() {
+  const client = await mongodb.MongoClient.connect(
+   'mongodb+srv://saturnV:apollo11@cluster0.pne15.mongodb.net/̣',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true 
+    }
+  );
+
+return client.db('Dragon').collection('Authors');
+}
+
+module.exports = router;
