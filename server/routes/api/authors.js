4d140e48340cbac67c106aca555136863cecaa47
Satya Perry <s.dog@satyadarshin.com>
2020-07-19 17:59:10 +0100

    Attempting to refine search results from MongoDB: not succeding in building passing right varaibles into find()

--- server/routes/api/authors.js
+++ server/routes/api/authors.js
@@ -5,41 +5,27 @@ const router = express.Router();
 
 // Get authors
 router.get('/', async (req, res) => {
-  // res.send('go, flight');
-  const authors = await loadAuthorsCollection();
-  res.send(await authors.find({}).toArray());
+  // const query = req.query;
+  // const parameter = Object.keys(query);
+  // const value = Object.values(query);
+  try {
+    const authors = await loadAuthorsCollection();
+    res.send(await authors.find({}).toArray());
+  } catch (error) {
+    return console.log(error)  
+  }
 });
 
-// Add author
-// router.author('/', async (req, res) => {
-//   const authors = await loadAuthorsCollection();
-//   await authors.insertOne({
-//     text: req.body.text,
-//     createdAt: new Date()
-//   });
-//   res.status(201).send();
-// });
-
-// // Delete author
-// router.delete('/:id', async (req, res) => {
-//   const authors = await loadAuthorsCollection();
-//   await authors.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
-//   res.status(200).send({});
-// });
-
 async function loadAuthorsCollection() {
-  const username = 'saturnV';
-  const password = 'apollo11';
   const client = await mongodb.MongoClient.connect(
     // Username: password pair redacted from the Github repo for security
-   `mongodb+srv://${username}:${password}@cluster0.pne15.mongodb.net/̣`,
+   `mongodb+srv://${process.env.VUE_APP_MONGODB_USER}:${process.env.VUE_APP_MONGODB_PASSWORD}@cluster0.pne15.mongodb.net/̣`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true 
     }
   );
-
-return client.db('Dragon').collection('Authors');
+  return client.db('Dragon').collection('Authors');
 }
 
 module.exports = router;
