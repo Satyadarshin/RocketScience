e3c93999f45fc0de33117ec7ae597d45bcdf80fa
Satya Perry <s.dog@satyadarshin.com>
2020-08-04 20:14:53 +0100

    Merge into master

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 const Author = require('./author');
 const Text = require('./text');
 
-const connectDb = () => {
-    return mongoose.connect(process.env.VUE_APP_MONGODB_CNCT);
-};
-
-const models = {Author, Text};
-
-//export {connectDb}; this is ES6 so won't work
-
-// export default; {models}; this is ES6 so won't work
-
-module.exports = models;
+module.exports = {
+    connectDb: function() {
+        return mongoose.connect(process.env.VUE_APP_MONGODB_CNCT, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true 
+        });
+    },
+    models: {
+        Author, 
+        Text
+    }
+}
--- server/routes/api/authors.js
+++ server/routes/api/authors.js
@@ -1,29 +1,16 @@
 const express = require('express');
-const mongodb = require('mongodb');
-//const mongoose = require('mongoose');
-// const mongoDB = `mongodb+srv://${process.env.VUE_APP_MONGODB_USER}:${process.env.VUE_APP_MONGODB_PASSWORD}@cluster0.pne15.mongodb.net/̣`
+//const mongodb = require('mongodb');
+const {connectDb, models} = require('../../models');
 
-// mongoose.connect(mongoDB, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true 
-// })
+const mongoose = require('mongoose');
+// const mongoDb = connectDb;
+// const app = express();
+const db = connectDb;
 
-// const db = mongoose.connection;
+
+
+// console.log(db);
 // db.on('error', console.error.bind(console, 'Connection error:'))
-
-// const authorSchema = new mongoose.Schema({
-//   // "id": 100,
-//   name: String,
-//   born: Date,
-//   died: Date,
-//   biography: String,
-//   website: [  {LinkText: String, url: String} ],
-//   gender: String,
-//   ethnicity: String,
-//   origin: String,
-//   // texts: [ 1000 ]
-// })
-// const Author = mongoose.model('Author', authorSchema)
 
 // Author.find((err, Authors)=> {
 //   if (err) return console.error(err)
@@ -41,6 +28,7 @@ router.get('/', async (req, res) => {
   const query = req.query;
   try {
     const authors = await loadAuthorsCollection();
+    console.log(authors);
     res.send(await authors.find(query).toArray());
   } catch (error) {
     return console.log(error)  
@@ -48,14 +36,14 @@ router.get('/', async (req, res) => {
 });
 
 async function loadAuthorsCollection() {
-  const client = await mongodb.MongoClient.connect(
-    // Username: password pair redacted from the Github repo for security
-   `mongodb+srv://${process.env.VUE_APP_MONGODB_USER}:${process.env.VUE_APP_MONGODB_PASSWORD}@cluster0.pne15.mongodb.net/̣`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true 
-    }
-  );
-  return client.db('Dragon').collection('Authors');
+  // const client = await connectDb.MongoClient.connect(
+  //   );
+  db().then(async () => {
+  express.listen(process.env.PORT, function() {
+    console.log('in');
+  })
+  });
+  // return client.db('Dragon').collection('Authors');
 }
 
 module.exports = router;
